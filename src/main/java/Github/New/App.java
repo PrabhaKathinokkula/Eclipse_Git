f193217834f47775555fb8d2b49f52f536c2fa85
SuryaPrabhaKathinokkula <kathinokkulabhavani2006@gmail.com>
2026-02-14 15:05:43 +0530

    second commit

--- src/main/java/Github/New/App.java
+++ src/main/java/Github/New/App.java
@@ -5,6 +5,6 @@ package Github.New;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Jenkins");
     }
 }
