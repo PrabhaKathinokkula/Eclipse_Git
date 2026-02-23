a9f70b9e499eb83a9d2a4f21c7ef925440ef860a
SuryaPrabhaKathinokkula <kathinokkulabhavani2006@gmail.com>
2026-02-23 23:16:34 +0530

    Removed Comments for testcases

--- src/test/java/Github/New/CseDemoTest.java
+++ src/test/java/Github/New/CseDemoTest.java
@@ -13,9 +13,9 @@ public class CseDemoTest {
         assertEquals(5, result);
     }
 
-  /* @Test
+   @Test
     void testMul() {
        int result = cal.mul(2, 3);
        assertEquals(6, result); 
-    } */
+    } 
 } 
